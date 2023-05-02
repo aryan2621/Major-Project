afaa09983b9bbafacf8487e5f1c3ebcb0a23c3cc
Rishabh Verma <aryanverma2412@gmail.com>
2023-05-03 01:08:44 +0530

    Updated testimonial

--- src/utils/team-members.ts
+++ src/utils/team-members.ts
@@ -14,18 +14,18 @@ export const TeamMembers: {
     name: 'Buddarapu Pravallika',
     role: 'Content writer',
     image: 'assets/members/pravallika.jpeg',
-    desc: 'I am Buddarapu Pravallika pursuing btech from Dr. B R Ambedkar National Institute of Technology in the stream of Mechanical Engineering.',
+    desc: 'Pursuing btech from Dr. B R Ambedkar NIT Jalandhar in the stream of Mechanical Engineering.',
   },
   {
     name: 'Praduman',
     role: 'Tester',
     image: 'assets/members/praduman.jpg',
-    desc: 'Hi, My name is Pradhayumn Soni. I am pursuing my B.Tech in Mechanical Engineering from NIT Jalandhar. My work in this project is testing and debugging.',
+    desc: 'Pursuing my B.Tech in Mechanical Engineering from NIT Jalandhar. My work in this project is testing and debugging in ML Model website.',
   },
   {
     name: 'Anand Kumar',
     role: 'Researcher',
     image: 'assets/members/anand.jpg',
-    desc: 'Hi, My name is Anand Pandey. I am pursuing my B.Tech in Mechanical Engineering from NIT Jalandhar. Working on this project as a research work on various soil components & elements affecting crop growth.',
+    desc: 'Pursuing my B.Tech in ME from NIT Jalandhar. Working on this project as a research work on soil components & elements affecting crop growth.',
   },
 ];
